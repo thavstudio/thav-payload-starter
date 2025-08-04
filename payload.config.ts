32259509e4101b1e8dc2dab4bd81040ce2830465
YoungNaga <founder@thavstudio.com>
2025-08-05 01:51:10 +0700

    Update payload.config.ts

--- payload.config.ts
+++ payload.config.ts
@@ -1,4 +1,4 @@
-import { buildConfig } from 'payload/config';
+import { buildConfig } from 'payload';
 import ExampleCollection from './src/collections/ExampleCollection';
 import ExampleGlobal from './src/globals/ExampleGlobal';
 
